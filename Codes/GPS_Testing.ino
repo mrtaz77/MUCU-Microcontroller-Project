d0595c552a4eaa604099f31ee480833edeac454e
mrtaz77 <mrtazwar23@gmail.com>
2024-03-26 16:46:47 +0600

    remove unnecessary files

--- Codes/GPS_Testing.ino
+++ Codes/GPS_Testing.ino
@@ -1,25 +1,29 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 TinyGPSPlus gps;
-SoftwareSerial mygps(4,3); // GPS Tx Pin - D4  GPS Rx Pin D3
+SoftwareSerial mygps(4, 3); // GPS Tx Pin - D4  GPS Rx Pin D3
 
-void setup(){
-  Serial.begin(9600);
-  mygps.begin(9600);
+void setup()
+{
+	Serial.begin(9600);
+	mygps.begin(9600);
 }
 
-void loop(){
-  Serial.println("Before gps");
-  // This sketch displays information every time a new sentence is correctly encoded.
-  while (mygps.available() > 0){
-    Serial.println("my gps available");
-    gps.encode(mygps.read());
-    Serial.println("after gps encode");
-    if (gps.location.isUpdated()){
-      Serial.print("Latitude= "); 
-      Serial.print(gps.location.lat(), 6);
-      Serial.print(" Longitude= "); 
-      Serial.println(gps.location.lng(), 6);
-      }
-    }
+void loop()
+{
+	Serial.println("Before gps");
+	// This sketch displays information every time a new sentence is correctly encoded.
+	while (mygps.available() > 0)
+	{
+		Serial.println("my gps available");
+		gps.encode(mygps.read());
+		Serial.println("after gps encode");
+		if (gps.location.isUpdated())
+		{
+			Serial.print("Latitude= ");
+			Serial.print(gps.location.lat(), 6);
+			Serial.print(" Longitude= ");
+			Serial.println(gps.location.lng(), 6);
+		}
+	}
 }
